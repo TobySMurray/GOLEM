a8716247ec067278a128f5b899991ca1fb3ac9a4
LeeZeitz <andrew.l.zeitz@gmail.com>
2021-04-24 21:21:27 -0700

    change transcend color on enemy

--- Scripts/BloodMoon.gd
+++ Scripts/BloodMoon.gd
@@ -4,7 +4,8 @@ var selected_enemy
 signal selected_enemy_signal
 
 func _ready():
-	self.connect("selected_enemy_signal", self.get_parent(), "toggle_selected_enemy")
+	print(self.get_parent().get_parent())
+	self.connect("selected_enemy_signal", get_node("../../Transcender"), "toggle_selected_enemy")
 
 func _on_Area2D_body_entered(body):
 	if visible and body.is_in_group("enemy"):
--- Scripts/Transcender.gd
+++ Scripts/Transcender.gd
@@ -1,13 +1,14 @@
 extends Node2D
 
 var transcender_curve = Curve2D.new()
-var should_draw
+var should_draw = false
+var enemy_is_selected = false
 
 func _draw():
 	if should_draw:
 		var curve_points = transcender_curve.tessellate()
 		for i in len(curve_points) - 1:
-			draw_line(curve_points[i], curve_points[i + 1], Color.crimson, 2)
+			draw_line(curve_points[i], curve_points[i + 1], (Color.crimson if enemy_is_selected else Color(0.86, 0.08, 0.24, 0.3)), 2)
 
 func draw_transcender(curve):
 	should_draw = true
@@ -17,3 +18,6 @@ func draw_transcender(curve):
 func clear_transcender():
 	should_draw = false
 	update()
+
+func selected_enemy_signal(enemy_is_selected):
+	enemy_is_selected = enemy_is_selected
