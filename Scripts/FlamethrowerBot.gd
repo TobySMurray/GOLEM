6fd12676abde20f52a15d5ac4021078609677118
Manoman017 <tobystefan@gmail.com>
2021-04-24 23:48:27 -0700

    added chainbot landminebot and flamethrower attacks

--- Scripts/FlamethrowerBot.gd
+++ Scripts/FlamethrowerBot.gd
@@ -1,12 +1,54 @@
 extends "res://Scripts/Enemy.gd"
 
+var num_pellets = 5
+
+var shot_spread = 15
+var shot_speed = 100
+var walk_speed = 100
+
+var fuel = 200
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	bullet_spawn_offset = 10
 	flip_offset = -46
-	pass # Replace with function body.
+
+func player_action():
+	aim_direction.y = 0
+	if Input.is_action_just_pressed("attack1") and attack_cooldown < 0:
+		fuel = 200
+		attacking = true
+		lock_aim = true
+		max_speed = 0
+		animplayer.play("Charge")
+	if Input.is_action_just_released("attack1"):
+		animplayer.play("Cooldown")
+		
+
+func _physics_process(delta):
+	if fuel <= 0:
+		animplayer.play("Cooldown")
+	if attacking:
+		fuel -= 1
+
+func attack():
+	attack_cooldown = 5
+	animplayer.play("Attack")
+	flamethrower()
+	
+func flamethrower():
+	for i in range(num_pellets):
+		if fuel > 0:
+			var pellet_dir = aim_direction.rotated((randf()-0.5)*deg2rad(shot_spread))
+			var pellet_speed = shot_speed * (1 + 0.5*(randf()-0.5))
+			shoot_bullet(pellet_dir*pellet_speed, 10, 0.8)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "Charge":
+		attack()
+	if anim_name == "Cooldown":
+		attacking = false
+		lock_aim = false
+		max_speed = 100
--- Scripts/LandmineBot.gd
+++ Scripts/LandmineBot.gd
@@ -1,13 +1,34 @@
 extends "res://Scripts/Enemy.gd"
 
 
-
-
-
 func _ready():
+	max_speed = 50
 	flip_offset = -6
+	attacking = false
+
+func player_action():
+	if Input.is_action_just_pressed("attack1") and not attacking:
+		attacking = true
+		lock_aim = true
+		max_speed = 0
+		attack_cooldown = 4
+		bury()
+		
+	if Input.is_action_just_pressed("attack2") and animplayer.current_animation == "BuryIdle":
+		animplayer.play("Undig")
+		
+	if Input.is_action_just_pressed("attack1") and animplayer.current_animation == "BuryIdle":
+		animplayer.play("Explode")
+
+func bury():
+	animplayer.play("Dig")
+	
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "Dig" and attacking:
+		animplayer.play("BuryIdle")
+	if anim_name == "Undig":
+		max_speed = 100 
+		attacking = false
+		lock_aim = false
