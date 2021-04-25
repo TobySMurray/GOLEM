28f8c71cccf4d6494339d88cd67f94f5db1df2a3
Manoman017 <tobystefan@gmail.com>
2021-04-24 20:08:27 -0700

    added worldobjects, map, and began swapbar

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -3,6 +3,9 @@ extends Node
 var timescale = 1
 var target_timescale = 1
 
+var swappable = false
+
+
 func _process(delta):
 	timescale = lerp(timescale, target_timescale, delta*12)
 	Engine.time_scale =  timescale
@@ -11,5 +14,6 @@ func lerp_to_timescale(scale):
 	target_timescale = scale
 	
 	
-
+func out_of_control():
+	pass
 	
---
+++ Scripts/SorcererBot.gd
@@ -0,0 +1,15 @@
+extends "res://Scripts/Enemy.gd"
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	flip_offset = -13
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
