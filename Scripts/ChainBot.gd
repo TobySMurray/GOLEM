dc0b403dd0c3868921621e6831bf0b4df2495813
Manoman017 <tobystefan@gmail.com>
2021-04-24 21:53:51 -0700

    test

--- Scripts/ChainBot.gd
+++ Scripts/ChainBot.gd
@@ -1,12 +1,24 @@
 extends "res://Scripts/Enemy.gd"
 
-
+var num_pellets = 6
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	max_speed = 100
+	bullet_spawn_offset = 12
 	flip_offset = 0
-	pass # Replace with function body.
 
+func player_action():
+	if Input.is_action_just_pressed("attack1") and attack_cooldown < 0:
+		attack()
+		
+func attack():
+	attacking = true
+	attack_cooldown = 150
+	animplayer.play("Attack")
+	var angle = 0
+	for i in num_pellets:
+		var pellet_direction = aim_direction.x.rotated(angle)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "Attack":
+		attacking = false
