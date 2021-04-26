93b73556e8a6f9f0155474d197156c01d04f402d
Manoman017 <tobystefan@gmail.com>
2021-04-25 20:46:59 -0700

    added bloodmoon fade

--- Scripts/BloodMoon.gd
+++ Scripts/BloodMoon.gd
@@ -2,10 +2,18 @@ extends AnimatedSprite
 
 var selected_enemy
 signal selected_enemy_signal
+onready var stopped = $Stopped
+var moon_visible = false
 
 func _ready():
 	print(self.get_parent().get_parent())
 	self.connect("selected_enemy_signal", GameManager.transcender, "toggle_selected_enemy")
+
+func _physics_process(delta):
+	if moon_visible:
+		modulate = lerp(modulate, Color(1,1,1,1), 0.1)
+	if !moon_visible:
+		modulate = lerp(modulate, Color(1,1,1,0), 0.2)
 
 func _on_Area2D_body_entered(body):
 	if visible and body.is_in_group("enemy"):
@@ -20,3 +28,10 @@ func _on_Area2D_body_exited(body):
 func emit_selected_enemy_signal(state):
 	self.modulate.a = 1 if state else 0.3
 	emit_signal("selected_enemy_signal", state)
+
+func _on_Slow_finished():
+	stopped.play()
+
+
+func _on_Speed_finished():
+	stopped.stop()
